86dda30fb84a2336fc74bdc903e186951ab18a13
brian witt <bwitt@value.net>
2014-10-14 18:43:24 -0700

    Add SynchronizedByteBuffer and test class.
    Signed-off-by: brian witt <bwitt@value.net>

---
+++ SRC/CrunchNoodlesCommon/src/com/crunchynoodles/util/SynchronizedByteBuffer.java
@@ -0,0 +1,231 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.crunchynoodles.util;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ *  A very simple buffer that does NOT wrap but can be enabled or disabled (to block new
+ *  {@link SynchronizedByteQueue#add(byte) } calls.
+ *  The buffer that is filled one {@code byte} at a time , but returns a byte-array
+ *  when requested to give up its content.
+ *  If first-byte matching is enabled, then initial bytes that do not match are ignored.
+ *
+ * <p> Since Java doesn't permit primitive types for type-parameters,
+ *  {@code byte} is part of class name.
+ *
+ * @author brian
+ */
+public class SynchronizedByteBuffer
+{
+
+    public SynchronizedByteBuffer( int maxSize )
+    {
+        m_count = new AtomicInteger();
+        m_enabled = new AtomicBoolean( false );
+        m_array = new byte[ maxSize ];
+        m_match_first_byte = -1;
+    }
+
+    //-----------------------  Buffer Data Management  ------------------------
+
+    /***
+     *  Resets buffer so it is holding zero bytes.
+     *  Notifies any threads awaiting change in 'count'.
+     *  Does not change enabled/disabled state.
+     */
+    public void     clear()
+    {
+        synchronized( m_count )
+        {
+            m_count.set( 0 );
+            m_count.notifyAll();
+        }
+    }
+
+    /***
+     *  Appends {@code ch} to end of buffer, incrementing count, and notifying any
+     *  threads waiting for a change to 'count'.
+     *  Only stores when the buffer is 'enabled'.
+     *
+     * <p> Throws illegal-array-index runtime error if buffer storage overflows.
+     *  If {@code ch} successfully added, notifies all threads waiting for a change of 'count'.
+     *
+     * @param ch byte to append to buffer.
+     */
+    public void     add( byte ch )
+    {
+        if( m_enabled.get() )
+        {
+            synchronized( m_count )
+            {
+                int  i = m_count.get();
+
+                if( i == 0 && m_match_first_byte != -1 && ch != m_match_first_byte )
+                {
+                    // await STX char...
+                }
+                else
+                {
+                    m_count.incrementAndGet();
+                    m_array[ i ] = ch;
+                    m_count.notifyAll();
+                }
+            }
+        }
+        //--- DEBUG vvv
+        else {
+            System.out.printf( "#add(%02x) while disabled, ignored.\n", ch );
+        }
+        //--- DEBUG ^^^
+    }
+
+    /***
+     *  Disable the accumulation of data and return an array of bytes {@link #add(byte) }'ed.
+     *  If no bytes have been add()'ed, then a zero-length byte array is returned.
+     *  Caller owns the returned byte-array.
+     *
+     * <p> Notifies all threads waiting on change of 'count' since afterwards it will be zero.
+     *
+     * @return byte-array of correct length, never {@code null}.
+     */
+    public byte[]   disableAndGet()
+    {
+        synchronized( m_count )
+        {
+            m_enabled.set( false );
+            byte[]  bunch = Arrays.copyOf( m_array, m_count.getAndSet( 0 ) );
+            m_count.notifyAll();
+
+            return( bunch );
+        }
+    }
+
+    //---------------------------  Concurrent Synchronization  ---------------------------
+
+    /***
+     *  Waits the caller until either a minimum number of chars received, or a timeout is reached.
+     *  If {@code timeoutMilliseconds} is zero, then no waiting occurs.
+     *
+     * <p> Uses {@link ManagementFactory#getRuntimeMXBean() } so time is monotonically increasing.
+     *  Using {@link System#currentTimeMillis() }, then time can skip forward, like for daylight savings time.
+     *  In Java, the granularity of timers is not specified.
+     *
+     * <p> Seehttp://stackoverflow.com/questions/817801/time-since-jvm-started
+     *
+     * @param minCount minimum chars in buffer before returning.
+     * @param timeoutMilliseconds maximum time to wait before returning, in milliseconds,
+     *              or zero to just test without waiting.
+     * @return false if sufficient chars received ; true if timeout occurred first.
+     * @throws java.lang.InterruptedException When time to exit thread...
+     */
+    public boolean  awaitCountAtLeast( int minCount, int timeoutMilliseconds )
+            throws InterruptedException
+    {
+        final RuntimeMXBean  runtimeMXBean = ManagementFactory.getRuntimeMXBean();
+
+        long  remaining = timeoutMilliseconds;
+        long  next_start = runtimeMXBean.getUptime();
+
+        //  We do some gymnastics with the timeout here.  m_count.wait(0) ::=
+        //  "If timeout is zero, however, then real time is not taken into consideration
+        //   and the thread simply waits until notified."
+        //
+        //  However, for this method "timeoutMilliseconds == 0" means non-block test.
+
+        if( remaining > 0 )
+        {
+            do
+            {
+                long  start_time = next_start;
+                synchronized( m_count )
+                {
+                    m_count.wait( remaining );
+                }
+
+                next_start = runtimeMXBean.getUptime();
+                remaining -= (next_start - start_time);
+            } while( remaining > 0 && getCount() < minCount );
+        }
+
+        return ( getCount() < minCount );
+    }
+
+    //-----------------------------  Bean Things  -----------------------------
+
+    /***
+     *  Returns count of bytes available, which is only stable if {@link #add(byte)}-ing has been disabled.
+     * @return current count of bytes available.
+     */
+    public int     getCount()
+    {
+        synchronized( m_count )
+        {
+            return m_count.get();
+        }
+    }
+
+    /***
+     *  Returns whether or not accumulation is enabled.
+     *  When disabled, no new bytes can be added to the buffer.
+     * @return {@code true} if queuing is enabled.
+     */
+    public boolean  getEnabled()
+    {
+        synchronized( m_enabled )
+        {
+            return m_enabled.get();
+        }
+    }
+
+    /***
+     *  Change if accumulation of bytes is enabled or disabled.
+     *  When disabled, no new bytes can be added to the buffer.
+     *
+     * @param en {@true} to permit queuing of incoming bytes.
+     */
+    public void    setEnabled( boolean en )
+    {
+        synchronized( m_enabled )
+        {
+            m_enabled.set( en );
+        }
+    }
+
+    /***
+     *  Returns first byte to match, or -1 if feature disabled.
+     * @return Byte to match, or -1 if not matching.
+     */
+    public int      getMatchFirstByte()
+    {
+        return m_match_first_byte;
+    }
+
+    /***
+     *  Sets the first byte stored, all other first bytes are ignored.
+     * @param ch0 Byte to match on, or -1 to turn off feature.
+     */
+    public void     setMatchFirstByte( int ch0 )
+    {
+        m_match_first_byte = ch0;
+    }
+
+    //-------------------------  Instance Variables  --------------------------
+
+    private final AtomicBoolean     m_enabled;
+
+    private final AtomicInteger     m_count;
+
+    private final byte[]            m_array;
+
+    private int                     m_match_first_byte;
+
+}
---
+++ SRC/CrunchNoodlesCommon/test/com/crunchynoodles/util/SynchronizedByteBufferTest.java
@@ -0,0 +1,242 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.crunchynoodles.util;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author brian
+ */
+public class SynchronizedByteBufferTest {
+
+    public SynchronizedByteBufferTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        System.out.println( "-- setUpClass() --" );
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        System.out.println( "-- tearDownClass() --" );
+    }
+
+    /**
+     * Test of clear method, of class SynchronizedByteBuffer.
+     */
+    @Test
+    public void testClear() {
+        System.out.println( "-- clear --" );
+        SynchronizedByteBuffer instance = new SynchronizedByteBuffer(55);
+        instance.setEnabled( true );
+        instance.clear();
+
+        int  cnt = instance.getCount();
+        assertTrue( cnt == 0 );
+    }
+
+    /**
+     * Test of add method, of class SynchronizedByteBuffer.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println( "-- add --" );
+        byte ch = 1;
+        SynchronizedByteBuffer instance = new SynchronizedByteBuffer(55);
+        instance.setEnabled( true );
+
+        instance.add( ch++ );
+        int  cnt = instance.getCount();
+        assertTrue( cnt ==  1 );
+
+        instance.add( ch++ );
+        cnt = instance.getCount();
+        assertTrue( cnt ==  2 );
+
+        instance.add( ch++ );
+        cnt = instance.getCount();
+        assertTrue( cnt ==  3 );
+    }
+
+    /**
+     * Test of disableAndGet method, of class SynchronizedByteBuffer.
+     */
+    @Test
+    public void testDisableAndGet() {
+        System.out.println( "-- disableAndGet --" );
+        SynchronizedByteBuffer instance = new SynchronizedByteBuffer(55);
+        instance.setEnabled( true );
+
+        //  Put in some content before doing disable.
+        byte  ch = 2;
+        instance.add(  ch++ );
+        instance.add(  ch++ );
+        instance.add(  ch++ );
+
+        byte[] expResult = { 2, 3, 4 };
+        byte[] result = instance.disableAndGet();
+        assertArrayEquals( expResult, result );
+        assertFalse( instance.getEnabled() );
+
+        //  buffer is disabled, so this add() will be ignored.
+        //  Since we just drained the buffer, it will have no content
+        instance.add( ch++ );
+        assertEquals( instance.getCount(), 0 );
+
+        result = instance.disableAndGet();
+        expResult = new byte[] { };     // empty.
+        assertArrayEquals( expResult, result );
+        assertFalse( instance.getEnabled() );
+
+    }
+
+    /**
+     * Test of awaitCountAtLeast method, of class SynchronizedByteBuffer.
+     */
+    @Test
+    public void testAwaitCountAtLeast()
+            throws Exception {
+        System.out.println( "-- awaitCountAtLeast --" );
+        SynchronizedByteBuffer instance = new SynchronizedByteBuffer(55);
+        instance.setEnabled( true );
+
+        //  "sufficient chars received" is 0
+        int timeoutMilliseconds = 1000;
+        boolean expResult = false;
+        boolean result = instance.awaitCountAtLeast( 0, timeoutMilliseconds );
+        assertEquals( expResult, result );
+
+        instance.add( (byte) 33 );
+        expResult = false;      // false means no timeout.
+        result = instance.awaitCountAtLeast( 1, timeoutMilliseconds );
+        assertEquals( expResult, result );
+
+        expResult = true;      // true means a timeout.
+        result = instance.awaitCountAtLeast( 5, timeoutMilliseconds );
+        assertEquals( expResult, result );
+
+    }
+
+    /**
+     * Test of getCount method, of class SynchronizedByteBuffer.
+     */
+    @Test
+    public void testGetCount() {
+        System.out.println( "-- getCount --" );
+        byte ch = 1;
+        SynchronizedByteBuffer instance = new SynchronizedByteBuffer(55);
+        instance.setEnabled( true );
+        assertTrue( instance.getEnabled() );
+
+        instance.add( ch++ );
+        int  cnt = instance.getCount();
+        assertTrue( cnt ==  1 );
+
+        instance.add( ch++ );
+        cnt = instance.getCount();
+        assertTrue( cnt ==  2 );
+
+        instance.add( ch++ );
+        cnt = instance.getCount();
+        assertTrue( cnt ==  3 );
+        assertTrue( instance.getEnabled() );
+    }
+
+    /**
+     * Test of getEnabled method, of class SynchronizedByteBuffer.
+     */
+    @Test
+    public void testGetEnabled() {
+        System.out.println( "-- getEnabled --" );
+        SynchronizedByteBuffer instance = new SynchronizedByteBuffer(55);
+        boolean expResult = false;
+        boolean result = instance.getEnabled();
+        assertEquals( expResult, result );
+
+        expResult = true;
+        instance.setEnabled( expResult );
+        result = instance.getEnabled();
+        assertEquals( expResult, result );
+    }
+
+    /**
+     * Test of setEnabled method, of class SynchronizedByteBuffer.
+     */
+    @Test
+    public void testSetEnabled() {
+        System.out.println( "-- setEnabled --" );
+        SynchronizedByteBuffer instance = new SynchronizedByteBuffer(55);
+        boolean expResult = false;
+        boolean result = instance.getEnabled();
+        assertEquals( expResult, result );
+
+        expResult = true;
+        instance.setEnabled( expResult );
+        result = instance.getEnabled();
+        assertEquals( expResult, result );
+
+        expResult = false;
+        instance.setEnabled( expResult );
+        result = instance.getEnabled();
+        assertEquals( expResult, result );
+
+    }
+
+    /**
+     * Test of getMatchFirstByte method, of class SynchronizedByteBuffer.
+     */
+    @Test
+    public void testGetMatchFirstByte() {
+        System.out.println( "-- getMatchFirstByte --" );
+        int ch0 = 0x7E;
+        SynchronizedByteBuffer instance = new SynchronizedByteBuffer(55);
+        instance.setEnabled( true );
+
+        //  Initially unset.
+        assertEquals( instance.getMatchFirstByte(), -1 );
+
+        instance.setMatchFirstByte( ch0 );
+        assertEquals( instance.getMatchFirstByte(), ch0 );
+
+        instance.add( (byte) 0x01 );
+        instance.add( (byte) 0x02 );
+        assertEquals( instance.getCount(), 0 );
+
+        instance.add( (byte) ch0 );
+        instance.add( (byte) ch0 );
+        instance.add( (byte) 0x81 );
+        instance.add( (byte) 0x82 );
+
+        assertEquals( instance.getCount(), 4 );
+
+        byte[]  expResult = { (byte) ch0, (byte) ch0, (byte) 0x81, (byte) 0x82 };
+        byte[]  result = instance.disableAndGet();
+        assertArrayEquals( expResult, result );
+    }
+
+    /**
+     * Test of setMatchFirstByte method, of class SynchronizedByteBuffer.
+     */
+    @Test
+    public void testSetMatchFirstByte() {
+        System.out.println( "-- setMatchFirstByte --" );
+        int ch0 = 0x7E;
+        SynchronizedByteBuffer instance = new SynchronizedByteBuffer(55);
+
+        //  Initially unset.
+        assertEquals( instance.getMatchFirstByte(), -1 );
+
+        instance.setMatchFirstByte( ch0 );
+        assertEquals( instance.getMatchFirstByte(), ch0 );
+    }
+
+}
